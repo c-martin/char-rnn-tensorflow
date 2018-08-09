9e37724143b56954dd43cb3ea0669995e8f2a1b3
cmartin <martin.caleb.j@gmail.com>
2018-08-09 12:47:09 -0400

    changed output behavior

--- sample.py
+++ sample.py
@@ -42,8 +42,8 @@ def sample(args):
         ckpt = tf.train.get_checkpoint_state(args.save_dir)
         if ckpt and ckpt.model_checkpoint_path:
             saver.restore(sess, ckpt.model_checkpoint_path)
-            print(model.sample(sess, chars, vocab, args.n, args.prime,
-                               args.sample).encode('utf-8'))
+            output = model.sample(sess, chars, vocab, args.n, args.prime, args.sample)
+            print(output)
 
 if __name__ == '__main__':
     sample(args)
